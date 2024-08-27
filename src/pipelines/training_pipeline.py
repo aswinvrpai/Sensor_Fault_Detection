3de9c65bb5a1b9e24cf6ac489ef1fd7d0bd7df3c
aswinp <aswinp@altair.com>
2024-08-27 14:34:47 +0900

    EDA started

---
+++ src/pipelines/training_pipeline.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+import sys
+
+# Data Ingestion code;
+sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)) + '/../../')
+from src.components.data_ingestion import DataIngestion
+sys.path.pop(1)
+
+
+if __name__ == "__main__":
+    
+    ingestion_obj=DataIngestion()
+    train_file,test_file=ingestion_obj.initiate_data_ingestion()
